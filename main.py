d4058465415bc54acfa59a442d19939b511baaa7
Maximo Dimas <156684286+MaxDimasDev@users.noreply.github.com>
2025-06-05 11:19:20 -0600

    Feat: 2. Cleaning

--- main.py
+++ main.py
@@ -6,3 +6,11 @@ df = pd.read_csv("unit1_practice_data.csv")
 #df["nombre de la nueva columna"] = df["nombre de la columna que queremos usar de referencia"].fillna(valor que queremos darle a los valores nulos)
 df["sales_filled"] = df["sales"].fillna(0)
 print(df)
+
+#2. Cleaning, remplazar valores negativos en cierta columna por NaN
+#df.loc para acceder a una fila o columna especifica
+#...[df["columna"] en donde el valor sea < 0 por ejemplo, "unicamente cambie el valor de la columna"]
+#...] = pd.NA esto para que le asigne un valor nulo a los valores que cumplan la condicion
+df.loc[df["discount"] <0, "discount"] = pd.NA 
+print(df)
+
